8b17e9b731b371597645b5ff51b363910cbb43cb
Y.Nanthachai <thecoachingonline@gmail.com>
2023-03-10 18:17:39 +0700

    Choosing what fortune

--- main.py
+++ main.py
@@ -75,3 +75,19 @@ if answer == 2:
     print("No")
 if answer == 3:
     print("Maybe")
+
+#Choosing what fortune to show
+import random
+
+lucky_number = random.randint(1,100)
+fortune_number = random.randint(1,3)
+fortune_text = ''
+
+if fortune_number == 1:
+    fortune_text = 'You will have a great day!'
+if fortune_number == 2:
+    fortune_text = 'Today will be tough...but worth it.'
+if fortune_number == 3:
+    fortune_text = 'You will get married this year!'
+
+print(f'{fortune_text} Your Lucky Number is: {lucky_number}')
