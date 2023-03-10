16b8c79120463936dc3039fd5aff591a49094e2f
Y.Nanthachai <thecoachingonline@gmail.com>
2023-03-10 19:18:13 +0700

    Add insert delete

--- main.py
+++ main.py
@@ -100,3 +100,15 @@ print(fav_movies[2])
 fav_numbers = [4,9,33]
 
 print(fav_numbers[0])
+
+#Add, insert, delete
+print(len(fav_movies))
+
+fav_movies.append("Iron Man")
+print(fav_movies)
+
+fav_movies.insert(1,"Batman")
+print(fav_movies)
+
+del(fav_movies[2])
+print(fav_movies)
