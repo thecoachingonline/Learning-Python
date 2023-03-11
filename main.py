06510fe199f32642e51f8a9f30b091976426f808
Y.Nanthachai <thecoachingonline@gmail.com>
2023-03-11 10:08:07 +0700

    Parameters

--- main.py
+++ main.py
@@ -220,6 +220,14 @@ def bark():
 for x in range(100):
     bark()
 
-def hello():
-    print("Hello Nick!")
-hello()
+#Parameters
+def hello(name):
+    print("Hello {name}!")
+
+hello("John")
+
+def add_numbers(num1,num2):
+    print(num1 + num2)
+
+add_numbers(4,8)
+add_numbers(3,7)
