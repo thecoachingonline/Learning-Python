9bcac39ff3f858a238538156963f2139ea037c1f
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-22 05:02:47 +0000

    helloworld

--- Basic/helloworld.py
+++ Basic/helloworld.py
@@ -1,3 +1,7 @@
-print("Hello World!")
-name = input("What is your name?")
-print("Nice to meet you", name)
+def main():
+   print("Hello World!")
+   name = input("What is your name?")
+   print("Nice to meet you", name)
+
+if __name__ == "__main__":
+   main()   
