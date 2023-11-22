9b785bd2b6eacc97504137becc788eaf7c2198e5
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-22 05:11:31 +0000

    Basic Data Types

---
+++ Basic/variable.py
@@ -0,0 +1,16 @@
+myint = 5
+myfloat = 13.2
+mystr = "This is a string"
+mybool = True
+mylist = [0, 1, "two", 3.2, False]
+mytuple = (0, 1, 2)
+mydict = {"one" : 1, "two" : 2}
+
+
+print(myint)
+print(myfloat)
+print(mystr)
+print(mybool)
+print(mylist)
+print(mytuple)
+print(mydict)
