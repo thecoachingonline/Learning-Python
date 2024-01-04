8014e6ca431cfd0e00e43cce4b3d1fb21cc1f5e4
Y.Nanthachai <thecoachingonline@gmail.com>
2024-01-04 04:05:44 +0000

    Collect input

--- MS-Python/program.py
+++ MS-Python/program.py
@@ -1,2 +1,20 @@
-sum = 1 + 2
-print(sum)
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+print("show this in the console")
+
+planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367 # float, lightyears
+can_liftoff = True
+shuttle_landed_on_the_moon = "Apollo 11" #string
+
+left_side = 10
+right_side = 5
+left_side / right_side # 2
+
+from datetime import date
+
+date.today()
+
+print("Today's date is: " + str(date.today()))
