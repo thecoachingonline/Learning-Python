82b98dbecda21ace8e8974d279aafd7ecb505702
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-14 03:54:52 +0000

    NOT Gate

---
+++ CSGates/NOT.py
@@ -0,0 +1,11 @@
+
+def NOT(a):
+	return not a
+
+if __name__=='__main__':
+	print(NOT(0))
+
+	print("+---------------+----------------+")
+	print(" | NOT Truth Table | Result |")
+	print(" A = False | A NOT =",NOT(False)," | ")
+	print(" A = True, | A NOT =",NOT(True)," | ")
