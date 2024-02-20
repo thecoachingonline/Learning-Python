7d14707e8b66c7d8fc458c0d901c1c3afb682966
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-20 05:41:19 +0000

    Exceptions

--- CS2024/calculator.py
+++ CS2024/calculator.py
@@ -1,5 +1,18 @@
+"""
 x = int(input("X: "))
 y = int(input("Y: "))
 
 z = x / y
 print(f"{z:.50f}")
+"""
+
+def get_int(prompt):
+    return int(input(prompt))
+
+def main():
+    x = get_int("x: ")
+    y = get_int("y: ")
+
+    print(x + y)
+
+main()
