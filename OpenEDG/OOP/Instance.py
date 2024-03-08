b62bd52d7e818e356d15ac1f7765387ff43200fc
Y.Nanthachai <thecoachingonline@gmail.com>
2024-03-08 09:51:35 +0000

    instanc

---
+++ OpenEDG/OOP/Instance.py
@@ -0,0 +1,11 @@
+class Book:
+
+    def __init__(self, title, author, pages, price):
+        self.title = title
+
+        self.author = author
+        self.pages = pages
+        self.price = price
+
+b1 = Book("War and Peace")
+b2 = Book("The Cather in the Rye")
