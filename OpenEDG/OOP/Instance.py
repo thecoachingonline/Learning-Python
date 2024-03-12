160b347e6bbb17f4de0c77ba943b58075c3caa31
Y.Nanthachai <thecoachingonline@gmail.com>
2024-03-12 04:06:29 +0000

    Price

--- OpenEDG/OOP/Instance.py
+++ OpenEDG/OOP/Instance.py
@@ -10,7 +10,7 @@ class Book:
     def getprice(self):
         return self.price
 
-b1 = Book("War and Peace")
-b2 = Book("The Cather in the Rye")
+b1 = Book("War and Peace","Leo Tolstoy", 1225, 39.95)
+b2 = Book("The Cather in the Rye","JD Salinger", 234, 29.95)
 
 print(b1.getprice())
