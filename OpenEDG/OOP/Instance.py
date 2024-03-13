f4483795bb4b6f12bdff16cd75eb866a0d95bbb1
Y.Nanthachai <thecoachingonline@gmail.com>
2024-03-13 03:44:38 +0000

    Amount

--- OpenEDG/OOP/Instance.py
+++ OpenEDG/OOP/Instance.py
@@ -8,7 +8,14 @@ class Book:
         self.price = price
 
     def getprice(self):
-        return self.price
+        if hasattr(self,"_discount"):
+            return self.price - (self.price * self._discount)
+        elif:
+            return self.price
+        
+    def setdiscount(self, amount):
+        self._discount = amount
+
 
 b1 = Book("War and Peace","Leo Tolstoy", 1225, 39.95)
 b2 = Book("The Cather in the Rye","JD Salinger", 234, 29.95)
