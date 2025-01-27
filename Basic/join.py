50252ecbfcd6353e25e727e0ee9ae8a8d3e7c258
Y.Nanthachai <thecoachingonline@gmail.com>
2025-01-27 09:11:25 +0000

    Join

---
+++ Basic/join.py
@@ -0,0 +1,14 @@
+# Define strings
+firstname = "Nanthachai"
+lastname = "Yuennan"
+
+# Define our sequence
+sequence = (firstname, lastname)
+
+# Join into new string
+name = " ".join(sequence)
+print(name)
+
+words = ["How","are","you","doing","?"]
+sequence = ' '.join(words)
+print(sequence)
