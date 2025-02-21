20adafd1c35507f9c9757fd0b4a31d0adadada26
Y.Nanthachai <thecoachingonline@gmail.com>
2025-02-21 07:40:28 +0000

    List

--- Basic/DataAndOperations/list.py
+++ Basic/DataAndOperations/list.py
@@ -7,3 +7,16 @@ ratings = [ 'A','A','B','A','C','A' ]
 print(ratings)
 print(ratings[0])
 print(ratings[3])
+
+list = [ "New York", "Los Angles", "Boston", "Denver" ]
+
+print(list)     # prints all elements
+print(list[0])  # print first element
+
+list2 = [1,3,4,6,4,7,8,2,3]
+
+print(sum(list2))
+print(min(list2))
+print(max(list2))
+print(list2[0])
+print(list2[-1])
