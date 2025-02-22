bf936f68077def22717034e29281cf70d483970f
Y.Nanthachai <thecoachingonline@gmail.com>
2025-02-22 06:35:36 +0000

    Sort List

---
+++ Basic/DataAndOperations/sortlist.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+x = [3,6,21,1,5,98,4,23,1,6]
+x.sort()
+print(x)
+
+words = ["Be","Car","Always","Door","Eat" ]
+words.sort()
+print(words)
+
+x = [3,6,21,1,5,98,4,23,1,6]
+x.sort()
+x = list(reversed(x))
+print(x)
+
+words = words[::-1]
