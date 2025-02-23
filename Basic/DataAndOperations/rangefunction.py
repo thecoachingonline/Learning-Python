ef4a9989cf296d416e9040a352dd88b275c6f741
Y.Nanthachai <thecoachingonline@gmail.com>
2025-02-23 12:21:46 +0000

    Range Function

---
+++ Basic/DataAndOperations/rangefunction.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#range(start, stop, step)
+
+x = list(range(100))
+print(x)
+
+x = list(range(1,101))
+print(x)
+
+for i in range(1,11):
+   print(i)
+
+for i in range(0,25,5):
+    print(i)
+
+for i in range(0,100,10):
+    print(i)
