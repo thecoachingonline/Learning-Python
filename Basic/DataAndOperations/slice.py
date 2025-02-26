585d6660179b968b35a9c35f7e1e3182ba38f71e
Y.Nanthachai <thecoachingonline@gmail.com>
2025-02-26 02:38:01 +0000

    Slice

---
+++ Basic/DataAndOperations/slice.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+persons = [ "John", "Marissa", "Pete", "Dayton" ]
+
+slice = persons[0:2]
+print(slice)
+
+destination = "summer holiday at beach"
+mySlice = destination[0:6]
+print(mySlice)
