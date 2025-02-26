2cf2d5e575b0059080590910c11ced7f06da0b8a
Y.Nanthachai <thecoachingonline@gmail.com>
2025-02-26 02:41:46 +0000

    Multiple Return

---
+++ Basic/DataAndOperations/multiplereturn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def getPerson():
+    name = "Leona"
+    age = 35
+    country = "UK"
+    return name,age,country
+
+name,age,country = getPerson()
+print(name)
+print(age)
+print(country)
